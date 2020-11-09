630ab9d8780165c73be7b0512d6b77ad5c0cf9d4
ELIZA DOLLANI <elizadollani@gmail.com>
2020-11-09 12:25:33 +0100

    peso 1, 8, 16, 24

--- byte/Program.cs
+++ byte/Program.cs
@@ -6,18 +6,47 @@ namespace CalcoloDimensioniImmagine
 {
 	static void Main(string[] args)
 	{
-		int n1, n2;
-		Console.WriteLine("Inserisci il prim o numero");
-		n1 = Convert.ToInt32(Console.ReadLine());
+		 string risultato;
+		double calcolo = 0;
+		Console.WriteLine("Inserisci il primo numero");
+		 double n1 = Convert.ToDouble(Console.ReadLine());
 		Console.WriteLine("Inserisci il secondo numero");
-		n2 = Convert.ToInt32(Console.ReadLine());
-		int calcolo = ((n1*n2)*3)/1024;
-		Console.WriteLine($"L' immagine pesa {calcolo}KB");
+		double n2 = Convert.ToDouble(Console.ReadLine());
+		Console.WriteLine("Inserisci la grandezza 1, 8, 16, 24");
+		risultato = Console.ReadLine();
+			switch (risultato)
+			{
+				case "1":
+					calcolo = ((n1 * n2) / 8) / 1024;
+					Console.WriteLine($"L'Immagine pesa {calcolo}KB");
+					break;
+
+				case "8":
+					calcolo = (n1 * n2) / 1024;
+					Console.WriteLine($"L'Immagine pesa {calcolo}KB");
+					break;
+
+				case "16":
+					calcolo = ((n1 * n2) * 2) / 1024;
+					Console.WriteLine($"L'Immagine pesa {calcolo}KB");
+					break;
+
+				case "24":
+					calcolo = ((n1 * n2) * 3) / 1024;
+					Console.WriteLine($"L'Immagine pesa {calcolo}KB");
+					break;
+
+				default:
+					Console.WriteLine(" Attenzione questo codice vale per 1, 8 16, 24, inserire un numero corretto");
+					break;
+			}
 			Console.ReadLine();
 
 
 
 
-		}
+
+
+			}
 }
 }
