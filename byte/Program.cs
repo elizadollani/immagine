aeab86b9d7c4024e96a639e6c24b4581e5da4436
ELIZA DOLLANI <elizadollani@gmail.com>
2020-11-09 11:12:04 +0100

    conversione

--- byte/Program.cs
+++ byte/Program.cs
@@ -7,11 +7,17 @@ namespace CalcoloDimensioniImmagine
 	static void Main(string[] args)
 	{
 		int n1, n2;
-		double calcolo;
 		Console.WriteLine("Inserisci il prim o numero");
 		n1 = Convert.ToInt32(Console.ReadLine());
 		Console.WriteLine("Inserisci il secondo numero");
 		n2 = Convert.ToInt32(Console.ReadLine());
+		int calcolo = ((n1*n2)*3)/1024;
+		Console.WriteLine($"L' immagine pesa {calcolo}KB");
+			Console.ReadLine();
+
+
+
+
 		}
 }
 }
