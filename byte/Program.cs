fea2ad65903fed537b73f10fe294eedbf97d7dbf
ELIZA DOLLANI <elizadollani@gmail.com>
2020-11-09 10:49:36 +0100

    Aggiungere i file di progetto.

---
+++ byte/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CalcoloDimensioniImmagine
+{
+    class Program
+{
+	static void Main(string[] args)
+	{
+		int n1, n2;
+		double calcolo;
+		Console.WriteLine("Inserisci il prim o numero");
+		n1 = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("Inserisci il secondo numero");
+		n2 = Convert.ToInt32(Console.ReadLine());
+		}
+}
+}
